145dcb5948412793b3ff54af2a2035b72ee3ccaf
shay23bra <shay.bratslavsky@cs.colman.ac.il>
2021-09-11 14:44:41 +0300

    Reorgenize, typos, and privacy update

---
+++ Shop-Project/Migrations/20210626092146_test26.6.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop_Project.Migrations
+{
+    public partial class test266 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConsoleId",
+                table: "Accessory");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConsoleId",
+                table: "Accessory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
